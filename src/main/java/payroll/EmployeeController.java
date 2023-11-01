7d349fd32120e38cf3d0534aabb91fec333437f0
RickTroc <riccardo.trocano@gmail.com>
2023-11-01 22:33:32 +0100

    link semplification

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -1,24 +1,37 @@
 package payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class EmployeeController {
 
     private final EmployeeRepository repository;
+    
+    private final EmployeeModelAssembler assembler;
+   
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler employeeModelAssembler) {
         this.repository = repository;
+        this.assembler = employeeModelAssembler;
     }
 
 
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+            .map(assembler::toModel) //
+            .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
     // end::get-a -ggregate-root[]
 
@@ -29,12 +42,24 @@ public class EmployeeController {
 
     // Single item
 
-    @GetMapping("/employees/{id}")
+   /*
+     @GetMapping("/employees/{id}")
     Employee one(@PathVariable Long id) {
 
         return repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+    } 
+
+   */
+
+    @GetMapping("/employees/{id}")
+    EntityModel<Employee> one(@PathVariable Long id){
+        Employee employee = repository.findById(id)
+            .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return assembler.toModel(employee);
     }
+
+
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
